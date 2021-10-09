a83610d11cbe9f6bcd3c6b495c32167bf4f2ce2f
android_nihao@163.com <caijia3570346>
2021-10-09 10:13:44 +0800

    Initial commit

---
+++ app/src/main/java/com/hualu/zxinglib/MainActivity.java
@@ -0,0 +1,21 @@
+package com.hualu.zxinglib;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.os.Bundle;
+import com.hualu.zlib.activity.CaptureActivity;
+
+public class MainActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+
+  public void scan(View view) {
+    Intent i = new Intent(this, CaptureActivity.class);
+    startActivity(i);
+  }
+}
